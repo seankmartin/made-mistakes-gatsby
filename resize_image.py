96132b08dadb6482ea42803a03f10747f93a89ca
Sean Martin <martins7@tcd.ie>
2020-07-02 14:35:18 +0100

    Update images

--- resize_image.py
+++ resize_image.py
@@ -1,24 +1,44 @@
 from PIL import Image
 import os
+import argparse
 
 
 def downscale_folder(folder):
     pass
 
 
-def downscale_image(image, source_res=(1920, 1080), dest_res=(1600, 900)):
+def downscale_image(
+    image, source_res=(1920, 1080), dest_res=(1600, 900), overwrite=False
+):
     if (source_res is None) or (image.size == source_res):
         new_image = image.resize(dest_res)
         base, ext = os.path.splitext(image.filename)
-        print("Saving image to {}".format(base + "_downscale" + ext))
-        new_image.save(base + "_downscale" + ext)
+        if overwrite:
+            print("Overwriting image at {}".format(image.filename))
+            new_image.save(image.filename)
+        else:
+            print("Saving image to {}".format(base + "_downscale" + ext))
+            new_image.save(base + "_downscale" + ext)
 
 
 if __name__ == "__main__":
-    image_path = r"/home/sean/Repos/made-mistakes-gatsby/src/images/gatsby.jpg"
+    parser = argparse.ArgumentParser("Command line args")
+    parser.add_argument(
+        "--path",
+        "-p",
+        type=str,
+        required=True,
+        help="Path to the image or folder of images",
+    )
+    parser.add_argument(
+        "--in_place", "-i", action="store_true", help="In-place resizing"
+    )
+    parsed = parser.parse_args()
+    image_path = parsed.path
+    overwrite = parsed.in_place
     if os.path.isfile(image_path):
         image = Image.open(image_path)
-        downscale_image(image, source_res=None)
+        downscale_image(image, source_res=None, overwrite=overwrite)
     elif os.path.isdir(image_path):
         downscale_folder(image_path)
     else:
